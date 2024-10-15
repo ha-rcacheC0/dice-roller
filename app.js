50246e8960c1e105a0328f65adea28d4f0afb14c
Lucas Keller <148141760+ha-rcacheC0@users.noreply.github.com>
2024-10-15 11:56:51 -0500

    program

---
+++ app.js
@@ -0,0 +1,18 @@
+function rollDice() {
+  const numOfDice = document.getElementById("number-of-dice").value;
+  const diceResult = document.getElementById("dice-result");
+  const diceImages = document.getElementById("dice-images");
+  const values = [];
+  const images = [];
+
+  for (let i = 0; i < numOfDice; i++) {
+    const value = Math.floor(Math.random() * 6) + 1;
+    values.push(value);
+    images.push(
+      `<img src="imgs/dice${value}.webp" alt="Dice ${value} width="100" height="100">`
+    );
+  }
+
+  diceResult.textContent = `dice: ${values.join(", ")}`;
+  diceImages.innerHTML = images.join("");
+}
